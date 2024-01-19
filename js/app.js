bd35b8f37c4f87904ba7ebf08f434ee319c00866
Heejae Seo <heejaeseo@Heejaeui-MacBookAir.local>
2024-01-19 13:06:24 +0900

    home - getting the current scene value and displaying the active text for sticky sections

--- js/app.js
+++ js/app.js
@@ -45,13 +45,26 @@
 
  // 
  function setLayout() {
+  // 각 섹션의 높이 지정해주기
    for (let i = 0; i < sceneInfo.length; i++) {
     sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
     sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
    }
+
+   yOffset = window.scrollY;
+   let totalScrollHeight = 0;
+   for (let i = 0; i < sceneInfo.length; i++) {
+    totalScrollHeight += sceneInfo[i].scrollHeight;
+    if (totalScrollHeight >= yOffset) {
+     currentScene = i;
+     break;
+    }
+   }
+  document.body.setAttribute("id", `js-showScene${currentScene}`);
  }
 
  function scrollLoop() {
+  // 현재 스크롤 높이를 기준으로 scene 계산하기
   prevScrollHeight = 0;
   for (let i = 0; i < currentScene; i++) {
    prevScrollHeight += sceneInfo[i].scrollHeight;
@@ -59,18 +72,17 @@
 
   if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
    currentScene++;
+   document.body.setAttribute("id", `js-showScene${currentScene}`);
   }
 
   if (yOffset < prevScrollHeight) {
    if (yOffset === 0) return;
    currentScene--;
+   document.body.setAttribute("id", `js-showScene${currentScene}`);
   }
-  
-  console.log(currentScene);
-  const body = document.querySelector("body");
-  body.setAttribute("id", `js-showScene${currentScene}`);
  }
 
+ window.addEventListener('load', setLayout);
  window.addEventListener('resize', setLayout);
  window.addEventListener('scroll', () => {
   yOffset = window.scrollY;
