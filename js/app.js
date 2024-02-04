5496379a5c5de0cc4c063d49b46b2e3c07c4350f
Heejae Seo <heejaeseo@Heejaeui-MacBookAir.local>
2024-02-04 17:15:49 +0900

    home - applying disappearing/transforming animations to all the sticky messages in the 1st section, depending on the current vertical scroll position

--- js/app.js
+++ js/app.js
@@ -17,10 +17,15 @@
   stickyMsg13: document.querySelector('#js_scrollSection-0 #js_stickyMsg13')
   },
   values: {
-  stickyMsg11OpacityIn: [0, 1, { start: 0.1, end: 0.2 }],
-  stickyMsg11OpacityOut: [1, 0, { start: 0.25, end: 0.3 }],
-  stickyMsg12Opacity: [0, 1, { start: 0.3, end: 0.4 }],
-  stickyMsgTransform: ['translateY(0px)', 'translateY(-30px)']
+  stickyMsg11OpacityIn: [ 0, 1, { start: 0.1, end: 0.25 }],
+  stickyMsg11OpacityOut: [ 1, 0, { start: 0.3, end: 0.35 }],
+  stickyMsg11TransformIn: [ 5, -5, { start: 0.1, end: 0.35 } ],
+  stickyMsg12OpacityIn: [ 0, 1, { start: 0.35, end: 0.5 }],
+  stickyMsg12OpacityOut: [ 1, 0, { start: 0.55, end: 0.6 }],
+  stickyMsg12TransformIn: [ 5, -5, { start: 0.35, end: 0.6 } ],
+  stickyMsg13OpacityIn: [ 0, 1, { start: 0.6, end: 0.75 }],
+  stickyMsg13OpacityOut: [ 1, 0, { start: 0.8, end: 0.85 }],
+  stickyMsg13TransformIn: [ 5, -5, { start: 0.6, end: 0.85 } ],
   }
  },
  {
@@ -74,20 +79,23 @@
 
  function calcValue(sceneValues, currentYOffset) {
  // 재생구간 스크롤 위치를 절댓값으로 환산하기
- let rv;
  const scrollHeight = sceneInfo[currentScene].scrollHeight;
  const scrollPercentage = currentYOffset / scrollHeight;
 
- if( sceneValues.length === 3 ) {
+ // console.log(scrollPercentage);
+
+ let rv;
+
+ if (sceneValues.length === 3) {
   const animationScrollStart = scrollHeight * sceneValues[2].start;
   const animationScrollEnd = scrollHeight * sceneValues[2].end;
   const animationDuration = animationScrollEnd - animationScrollStart;
   
-  if ( currentYOffset >= animationScrollStart && currentYOffset <= animationScrollEnd ) {
-   rv = (( currentYOffset - animationScrollStart ) / animationDuration ) * (( sceneValues[1] - sceneValues[0]) + sceneValues[0]) ;
-  } else if ( currentYOffset < animationScrollStart ) {
+  if (currentYOffset >= animationScrollStart && currentYOffset <= animationScrollEnd) {
+   rv = (( currentYOffset - animationScrollStart ) / animationDuration ) * ( sceneValues[1] - sceneValues[0] ) + sceneValues[0] ;
+  } else if (currentYOffset < animationScrollStart) {
    rv = sceneValues[0];
-  } else {
+  } else if (currentYOffset > animationScrollEnd) {
    rv = sceneValues[1];
   }
  } else {
@@ -98,30 +106,57 @@
  }
 
  function playAnimation() {
- let sceneObjs = sceneInfo[currentScene].objs;
- let sceneValues = sceneInfo[currentScene].values;
- let currentYOffset = yOffset - prevScrollHeight;
- 
- switch (currentScene) {
-  case 0:
-  // 현재 scene 에 적용할 CSS 값 선언하기
-  let stickyMsg11Opacity = calcValue( sceneValues.stickyMsg11OpacityIn, currentYOffset );
-  
-  sceneObjs.stickyMsg11.style.opacity = stickyMsg11Opacity;
-  console.log( stickyMsg11Opacity );
+  const sceneObjs = sceneInfo[currentScene].objs;
+  const sceneValues = sceneInfo[currentScene].values;
+  const currentYOffset = yOffset - prevScrollHeight;
+  const scrollHeight = sceneInfo[currentScene].scrollHeight;
+  const scrollPercentage = currentYOffset / scrollHeight;
 
-  break;
-  case 1:
+  switch (currentScene) {
+   case 0:
+   // 현재 scene 에 적용할 CSS 값 선언하기
+   const msg11OpacityIn = calcValue( sceneValues.stickyMsg11OpacityIn, currentYOffset );
+   const msg11OpacityOut = calcValue( sceneValues.stickyMsg11OpacityOut, currentYOffset );
+   const msg11TransformIn = calcValue( sceneValues.stickyMsg11TransformIn, currentYOffset );
+   const msg12OpacityIn = calcValue( sceneValues.stickyMsg12OpacityIn, currentYOffset );
+   const msg12OpacityOut = calcValue( sceneValues.stickyMsg12OpacityOut, currentYOffset );
+   const msg12TransformIn = calcValue( sceneValues.stickyMsg12TransformIn, currentYOffset );
+   const msg13OpacityIn = calcValue( sceneValues.stickyMsg13OpacityIn, currentYOffset );
+   const msg13OpacityOut = calcValue( sceneValues.stickyMsg13OpacityOut, currentYOffset );
+   const msg13TransformIn = calcValue( sceneValues.stickyMsg13TransformIn, currentYOffset );
 
-  // console.log(`${currentScene} play`);
-  break;
-  case 2:
-  // console.log(`${currentScene} play`);
-  break;
-  case 3:
-  // console.log(`${currentScene} play`);
-  break;
- }
+   sceneObjs.stickyMsg11.style.transform = `translateY(${msg11TransformIn}%)`;
+   sceneObjs.stickyMsg12.style.transform = `translateY(${msg12TransformIn}%)`;
+   sceneObjs.stickyMsg13.style.transform = `translateY(${msg13TransformIn}%)`;
+
+   if (scrollPercentage <= 0.28) { 
+    sceneObjs.stickyMsg11.style.opacity = msg11OpacityIn;
+   } else { 
+   sceneObjs.stickyMsg11.style.opacity = msg11OpacityOut;
+   }
+   
+   if (scrollPercentage <= 0.53) { 
+    sceneObjs.stickyMsg12.style.opacity = msg12OpacityIn;
+   } else { 
+   sceneObjs.stickyMsg12.style.opacity = msg12OpacityOut;
+   }
+   if (scrollPercentage <= 0.78) { 
+    sceneObjs.stickyMsg13.style.opacity = msg13OpacityIn;
+   } else { 
+   sceneObjs.stickyMsg13.style.opacity = msg13OpacityOut;
+   }
+   
+   break;
+
+   case 1:
+   break;
+
+   case 2:
+   break;
+
+   case 3:
+   break;
+  }
  }
 
  function scrollLoop() {
